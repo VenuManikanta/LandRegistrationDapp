53e89464afe05f44de718b06fef05af6fd93683c
VenuManikanta <mastervenu@gmail.com>
2022-05-14 07:18:53 +0530

    Modified

---
+++ Server/backend/Config/db_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  email: "mastervenu18@gmail.com",
+  password: "",
+  NEXMO_API_KEY: "34bf759f",
+  NEXMO_API_SECRET: "4FGoQ5dhSbP3YJbG",
+  NEXMO_FROM_NUMBER: "",
+  MongoURI: `mongodb+srv://AbdurRehman03:de62pVeSMy3vSJY2@cluster0.149ot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+};
